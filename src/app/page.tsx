e658d3e282bed5a11585556d0de006d31cb828a1
Otthon Leão <otthonleao@hotmail.com>
2024-03-06 11:39:13 -0400

    build: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+			<h1>SMF - SISTEMA DE MOBILIDADE FLUVIAL</h1>
+			<h2>PÁGINA EM CONSTRUÇÃO</h2>
+			<h3>
+				<div>
+					<ul>
+						<li>ALAN</li>
+						<li>ELIAS EMANUEL CARVALHO LOPES</li>
+						<li>JAIRE PEREIRA DA SILVA</li>
+						<li>LAIRA KLISSIA MARQUES DE LIMA</li>
+						<li>MESSIAS ASSUNÇÃO SANTOS DO NASCIMENTO</li>
+						<li>OTTHON RODRIGO PAIXÃO LEÃO</li>
+					</ul>
+				</div>
+			</h3>
+    </main>
+  );
+}
